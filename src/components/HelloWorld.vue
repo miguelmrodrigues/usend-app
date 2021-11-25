b8a1884f2b2e5a4861c61fc44443ac4c7bc711d1
Miguel Rodrigues <rodriguesmmiguel@gmail.com>
2021-11-25 11:06:11 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    {{dashboard}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+      return {
+        dashboard: null
+      }
+  },
+  props: {
+    msg: String
+  },
+  created() {
+    // Simple GET request using fetch
+    fetch("https://thingproxy.freeboard.io/fetch/https://voz3p605f3.execute-api.us-east-1.amazonaws.com/Production/agent-app-dashboard")
+      .then(response => response.json())
+      .then(data => (this.dashboard = data));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
